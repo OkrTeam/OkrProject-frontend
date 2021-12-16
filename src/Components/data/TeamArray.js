09295982045a06d0bb8a940dc3daf909bb9a06bb
Михалев Кирилл <mihalov.k@mail.ru>
2021-12-16 23:00:05 +0500

    OkrTree remake

--- src/Components/data/TeamArray.js
+++ src/Components/data/TeamArray.js
@@ -1,3 +1,3 @@
-let TeamArray = [{name:"Team 1"},{name:"Team 2"},{name:"Team 3"},{name:"Team 4"},{name:"Team 5"},{name:"Team 6"}]
+let TeamArray = [{name:"Team 1"},{name:"Team 2"},{name:"Team 3"},{name:"Team 4"},{name:"Team 5"},{name:"Team 6"},{name:"Team 7"},{name:"Team 8"}]
 
 export default TeamArray;
