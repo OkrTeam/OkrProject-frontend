bf2494d9e6b55edfaa1c33aed45844ba22379fd9
Kolyganov Ivan <adamjensen2302@gmail.com>
2022-01-11 01:00:03 +0500

    Чекбоксы участникам, стиль страницы
    подключил react-multi-select-component для выбора участников, с помощью стилей бутстрапа сдвинул форму в середину экрана

--- src/Components/Teams/AddTeam.js
+++ src/Components/Teams/AddTeam.js
@@ -8,6 +8,8 @@ import 'bootstrap/js/dist/collapse.js'
 import 'bootstrap/js/dist/dropdown.js'
 import AddForm from "./AddForm";
 import freeParticipants from '../data/freeParticipants'
+import {MultiSelect} from "react-multi-select-component";
+import s from './teamsStyle.css'
 
 const departments = [{ name: "Dodo Engineering", teams: [{ name: "Команда-1", participants: ["Власов Иосиф", "Громов Варлам", "Герасимов Остап", "Кондратьев Антон", "Никитин Остап"] }, { name: "Команда-2", participants: ["Грибоедов Михаил", "Алексеев Артем", "Гришин Кассиан", "Лихачёв Дональд", "Кузнецов Осип"] }, { name: "Команда-3", participants: ["Гаврилов Кондратий", "Беспалов Герасим", "Константинов Арнольд", "Якушев Карл", "Комаров Панкрат"] }] },
                      { name: "Dodo Development", teams: [{ name: "Команда-4", participants: ["Савельев Давид", "Горбачёв Любомир", "Антонов Юлиан", "Нестеров Семен", "Шестаков Григорий"] }, { name: "Команда-5", participants: ["Ситников Геннадий", "Самсонов Филипп", "Молчанов Филипп", "Мишин Родион", "Корнилов Игорь"] }, { name: "Команда-6", participants: ["Герасимов Адам", "Панфилов Павел", "Пестов Аввакум", "Николаев Анатолий", "Дорофеев Лазарь"] }] }
@@ -18,8 +20,29 @@ function AddTeam() {
     const [name, setName] = useState('')
     const [department, setDepartment] = useState('')
     const [participants, setParticipants] = useState('')
+    const [toggle, setToggle] = useState('')
 
-    const [selectedFlavors, setSelectedFlavors] = useState([]);
+    const [selected, setSelected] = useState([])
+    const options = [
+        { label: "Свободный участник1", value: "1" },
+        { label: "Свободный участник2", value: "2" },
+        { label: "Свободный участник3", value: "3" },
+        { label: "Свободный участник4", value: "4" },
+        { label: "Свободный участник5", value: "5" }
+    ];
+    const overrideStrings = {
+        "allItemsAreSelected": "Все участники выбраны.",
+        "clearSearch": "Clear Search",
+        "clearSelected": "Clear Selected",
+        "noOptions": "No options",
+        "search": "Поиск",
+        "selectAll": "Выбрать всех",
+        "selectAllFiltered": "Select All (Filtered)",
+        "selectSomeItems": "Выберите участников",
+        "create": "Create",
+    }
+
+    const [selectedFlavors, setSelectedFlavors] = useState([])
 
     const handleSelect = function(selectedItems) {
         const members = [];
@@ -40,37 +63,36 @@ function AddTeam() {
         //Создание команды
     }
 
+    const handleToggle = ({ target }) =>
+        setToggle(s => ({ toggle, [target.name]: !toggle[target.name] }));
 
 
     return(
         <div>
             <CustomNavbar brand='Dodo OKR' myOKR='Мой OKR' teams='Команды' username='Username'/>
-            <form onSubmit={submitHandler}>
-                <input type='text' className='form-item form-control margin-bottom' name='name'
-                       onChange={event => setName(event.target.value)} placeholder='Название команды'/>
-                <input type='text' className='form-item form-control margin-bottom' name='name'
-                       onChange={event => setName(event.target.value)} placeholder='Номер Команды'/>
-                <select className="form-select form-control margin-bottom"  name='department'
-                        onChange={event => setDepartment(event.target.value)}>
-                    <option selected>Выберите подразделение</option>
-                    <option value="1">Dodo Engineering</option>
-                    <option value="2">Dodo 42</option>
-                </select>
-                <label>
-                    Выберите участников
-                    <select multiple={true} className="form-select form-control margin-bottom" value={participants}
-                            onChange={(e)=> {handleSelect(e.target.selectedOptions)}}>
-                        <option value="1"> Свободный участник1 </option>
-                        <option value="2"> Свободный участник2 </option>
-                        <option value="3"> Свободный участник3 </option>
-                        <option value="4"> Свободный участник4 </option>
-                        <option value="5"> Свободный участник5 </option>
+            <div className="col-lg-8 offset-2">
+                <form onSubmit={submitHandler}>
+                    <input type='text' className='text-center form-item form-control margin-bottom' name='name'
+                           onChange={event => setName(event.target.value)} placeholder='Название команды'/>
+                    <select className="text-center form-select form-control margin-bottom"  name='department'
+                            onChange={event => setDepartment(event.target.value)}>
+                        <option selected>Выберите подразделение</option>
+                        <option value="1">Dodo Engineering</option>
+                        <option value="2">Dodo 42</option>
                     </select>
-                </label>
-                <div className="">
-                    <input type="submit" className="btn add-team-btn round" value="Создать"/>
-                </div>
-            </form>
+                        <MultiSelect
+                                options={options}
+                                value={selected}
+                                onChange={setSelected}
+                                labelledBy={"Выберите участников"}
+                                overrideStrings={overrideStrings}
+                                className="text-center"
+                        />
+                    <div>
+                        <input type="submit" className="btn add-team-btn round" value="Создать"/>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
