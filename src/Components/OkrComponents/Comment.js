f369c131d944fed3b85ec329db09688c74925090
Михалев Кирилл <mihalov.k@mail.ru>
2022-01-17 21:38:25 +0500

    Okr card toggle fix

--- src/Components/OkrComponents/Comment.js
+++ src/Components/OkrComponents/Comment.js
@@ -1,19 +1,15 @@
-﻿import React from 'react'
+import React from 'react'
 import './targetList.css'
 
- function Comments(props){
-        return (
-            <>
-                {props.comments.map((comment) => {
-                    return (
-                        <div className="inrow comment-space-between comment-padding">
-                            <div className="font half">{comment.text}</div>
-                            <div class="comment-progress font">{comment.comprog}</div>
-                        </div>
-                    )
-                })}
-            </>
-        )
+function Comment(props){
+    return (
+        <>
+            <div className="inrow comment-space-between comment-padding">
+                <div className="font half">{props.text}</div>
+                <div class="comment-progress font">{props.comprog}</div>
+            </div>
+        </>
+    )
 }
 
-export default Comments
+export default Comment
---
+++ src/Components/OkrComponents/SubgoalList.js
@@ -0,0 +1,23 @@
+﻿import React, { useState } from 'react'
+import { Collapse, Button, CardBody, Card, Table } from 'reactstrap';
+import './targetList.css'
+import AddProgress from "../OkrComponents/AddProgress";
+import CommentList from "./CommentList";
+import Subgoal from "./Subgoal";
+
+function SubgoalList(props) {
+
+    return (
+            <>
+                {props.subgoals.map((subgoal) => {
+                    return(
+                        <Subgoal subgoalname={subgoal.subgoalname} progress={subgoal.progress}
+                                 keyres={subgoal.keyres} id={subgoal.id} comments={subgoal.comments}
+                                 q1={subgoal.q1} q2={subgoal.q2} q3={subgoal.q3} q4={subgoal.q4}/>
+                    )
+                })}
+            </>
+        )
+}
+
+export default SubgoalList
